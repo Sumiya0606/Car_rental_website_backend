a488caa794dd92b258dc80a67c9026f932a2e909
Sumiya <sumithaju143@gmail.com>
2024-06-25 07:03:24 +0400

    initial commit

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import adminRouter from './routes/adminRoutes.js'
 
 
 const corsOptions = {
-  origin:  'http://localhost:5173',
+  origin:  'https://car-rental-website-backend.onrender.com',
   credentials: true
 };
 
