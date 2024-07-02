49547603f60bc774552631129aa57257cd67efb9
Sumiya <sumithaju143@gmail.com>
2024-07-03 00:43:25 +0530

    initial commit

--- routes/paymentRotes.js
+++ routes/paymentRotes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {createRazorpayOrder,verifyRazorpayPayment} from '../controlers/paymentController'
+import {createRazorpayOrder,verifyRazorpayPayment} from '../controlers/paymentController.js'
 const router = express.Router();
 
 router.post("/create-order", createRazorpayOrder);
