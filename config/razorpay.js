e9e295176f9e81999fdd0857b9d1bf2d621e86ce
Sumiya <sumithaju143@gmail.com>
2024-07-04 04:06:06 +0530

    initial commit

--- config/razorpay.js
+++ config/razorpay.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv'
 
 import Razorpay from 'razorpay'
-dotenv.config()
+dotenv.config();
 const razorpayInstance = new Razorpay({
     key_id: process.env.KEY_ID,
     key_secret: process.env.KEY_SECRET,
